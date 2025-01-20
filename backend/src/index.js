d837fee1532f51a4a95bf07ffe9da156540e585e
jitendrasuthar1998 <jitendrasuthar995@gmail.com>
2025-01-20 23:46:19 +0530

    added signup, login, and logout routes

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,15 +1,25 @@
+import dotenv from 'dotenv';
 import express from "express";
 import authRoutes from "./routes/auth.route.js";
 import { connectDB } from "./lib/db.js";
-import dotenv from 'dotenv';
-dotenv.config();
 
+dotenv.config();
 const app = express();
 const PORT = process.env.PORT;
 
+// middleware allow to extract json from user request body
+app.use(express.json());
+
 app.use("/api/auth", authRoutes);
 
-app.listen(PORT, ()=> {
-    console.log("listening on",PORT)
+function testRoute(req, res) {
+    console.log("testRoute");
+}
+
+// app.get("/", testRoute);
+
+
+app.listen(8000, () => {
+    console.log("listening on", 8000)
     connectDB();
 });
