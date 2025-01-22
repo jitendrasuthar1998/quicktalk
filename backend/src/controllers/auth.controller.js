e94d2254008b6e611b2fd7c274e84db7f1a2524a
jitendrasuthar1998 <jitendrasuthar995@gmail.com>
2025-01-23 01:04:03 +0530

    created checkAuth route controller

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -100,3 +100,12 @@ export const updateProfile = async (req, res) => {
         return res.status(500).json({ message: "Internal Server Error" });
     }
 }
+
+export const checkAuth = (req, res) => {
+    try {
+        return res.status(200).json(req.user);
+    } catch (error) {
+        console.log("Error in checkAuth controller", error.message);
+        return res.status(500).json({ message: "Internal Server Error" });
+    }
+}
