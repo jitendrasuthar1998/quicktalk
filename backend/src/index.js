7410783b57dcb1154d8405d71689dba45fd9daf0
jitendrasuthar1998 <jitendrasuthar995@gmail.com>
2025-01-23 01:23:37 +0530

    added route to get all message between two users

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,6 +1,7 @@
 import dotenv from 'dotenv';
 import express from "express";
 import authRoutes from "./routes/auth.route.js";
+import messageRoutes from "./routes/message.route.js";
 import { connectDB } from "./lib/db.js";
 import cookieParser from "cookie-parser";
 
@@ -12,7 +13,7 @@ const PORT = process.env.PORT;
 app.use(express.json());
 app.use(cookieParser());
 app.use("/api/auth", authRoutes);
-
+app.use("/api/message", messageRoutes)
 function testRoute(req, res) {
     console.log("testRoute");
 }
