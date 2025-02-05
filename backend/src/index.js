6c455daf48514e711eb2660b61a9a5070652890a
jitendrasuthar1998 <jitendrasuthar995@gmail.com>
2025-02-05 20:47:30 +0530

    added SideNavbar, Navbar, ChatContainer components

--- backend/src/index.js
+++ backend/src/index.js
@@ -21,7 +21,7 @@ app.use(cookieParser());
 
 
 app.use("/api/auth", authRoutes);
-app.use("/api/message", messageRoutes)
+app.use("/api/messages", messageRoutes)
 function testRoute(req, res) {
     console.log("testRoute");
 }
